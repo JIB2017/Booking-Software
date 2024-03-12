523cd00f324ea2e57a8da1152c122a0376b37d0e
JIB2017 <blacuttjuan07@gmail.com>
2024-03-12 05:24:37 -0300

    Implementing a route for our admin

---
+++ src/server/api/routers/admin.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+import { SignJWT } from "jose";
+import { nanoid } from "nanoid";
+import { getJwtSecretKey } from "~/lib/auth";
+import cookie from "cookie";
+import { TRPCError } from "@trpc/server";
+
+const adminRouter = router({
+  login: publicProcedure
+    .input(z.object({ email: z.string().email(), password: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { res } = ctx;
+      const { email, password } = input;
+      const secret = new TextEncoder().encode(getJwtSecretKey());
+
+      if (input.email === email && input.password === password) {
+        const token = await new SignJWT({})
+          .setProtectedHeader({ alg: "HS256" })
+          .setJti(nanoid())
+          .setIssuedAt()
+          .setExpirationTime("2h")
+          .sign(secret);
+
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("user-token", token, {
+            httpOnly: true,
+            path: "/",
+            secure: process.env.NODE_ENV === "production",
+          }),
+        );
+
+        return { success: true };
+      }
+
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "Invalid email or password",
+      });
+    }),
+});
+
+export type AppRouter = typeof adminRouter;
