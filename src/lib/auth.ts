e1c3c903ca144e1f70365450fc6bd4a1e09e8343
JIB2017 <blacuttjuan07@gmail.com>
2024-03-17 23:51:34 -0300

    Added a function to verify token authenticity

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,10 @@
+import { jwtVerify } from "jose";
+
+interface JWTPayload {
+    jti: string,
+    iat: number
+}
+
 export function getJwtSecretKey(): string {
     const secret = process.env.JWT_SECRET_KEY;
 
@@ -6,4 +13,13 @@ export function getJwtSecretKey(): string {
     }
 
     return secret;
+}
+
+export const verifyAuth = async (token: string) => {
+    try {
+        const verified = await jwtVerify(token,  new TextEncoder().encode(getJwtSecretKey()));
+        return verified.payload as JWTPayload
+    } catch (e) {
+        throw new Error("Token not valid or expired.");
+    }
 }
